135898f49775601a59e21d80441faf4d19515d30
ilyashirko <ilyashirko@gmail.com>
2022-02-11 17:41:43 +0300

    latest launch replaced with all availible launches

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -1,3 +1,4 @@
+from asyncio import exceptions
 import requests
 
 from tqdm import tqdm
@@ -5,17 +6,26 @@ from tqdm import tqdm
 from main import download_file
 
 
-def main():
-    url = 'https://api.spacexdata.com/v3/launches/latest'
-
+def get_latest_launch():
+    url = 'https://api.spacexdata.com/v4/launches/latest'
     response = requests.get(url)
     response.raise_for_status()
+    return response.json()['flight_number']
 
+
+def get_photos(launch_num):
+    launch_url = f'https://api.spacexdata.com/v3/launches/{launch_num}'
+    response = requests.get(launch_url)
+    response.raise_for_status()
     for url in tqdm(response.json()['links']['flickr_images']):
-        download_file(f'images/spacex/{response.json()["flight_number"]}', url)
+        download_file(f'images/', url)
+
 
 if __name__ == '__main__':
-    try:
-        main()
-    except requests.exceptions.HTTPError as error:
-        print(f'ERROR!\n\n{error}')
+    latest_flight = get_latest_launch()
+    for flight_num in range(1, latest_flight + 1):
+        print(f'Checking {flight_num} flight...') 
+        try:
+            get_photos(flight_num)
+        except requests.exceptions.HTTPError as error:
+            print(error)
