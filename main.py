64fd9838483eb04451d937d490773f724a726309
ilyashirko <ilyashirko@gmail.com>
2022-02-12 19:54:01 +0300

    deleted extra function

--- main.py
+++ main.py
@@ -5,18 +5,13 @@ from urllib.parse import unquote, urlsplit
 import requests
 
 
-def create_directory(directory):
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-
 def download_file(new_file_path, url):
     new_file_name = os.path.split(unquote(urlsplit(url).path))[1]
 
     if os.path.exists(f'{new_file_path}/{new_file_name}'):
         return
 
-    create_directory(new_file_path)
+    os.makedirs(new_file_path, exist_ok=True)
 
     response = requests.get(url)
     response.raise_for_status()
