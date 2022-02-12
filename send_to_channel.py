c8bf6f5ca68fd018ff5eab3a144da2669d7e0f07
ilyashirko <ilyashirko@gmail.com>
2022-02-12 19:38:59 +0300

    checked by flake8

--- send_to_channel.py
+++ send_to_channel.py
@@ -13,16 +13,26 @@ def send_random_photo(bot, channel_id):
     photo = random.choice(os.listdir('images'))
     with open(f'images/{photo}', 'rb') as photo:
         bot.send_photo(chat_id=channel_id, photo=photo)
-        
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='App send one photo per day (or entered delay) as a telegram-bot to telegram channel')
-    parser.add_argument('delay', type=int, nargs='?', default=86400, help=('enter delay in seconds (default = 86400 sec.)'))
+    parser = argparse.ArgumentParser(
+        description=('App send one photo per day (or entered delay) '
+                     'as a telegram-bot to telegram channel')
+    )
+
+    parser.add_argument(
+        'delay',
+        type=int,
+        nargs='?',
+        default=86400,
+        help=('enter delay in seconds (default = 86400 sec.)')
+    )
     args = parser.parse_args()
 
     load_dotenv()
     telegram_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
-    channel_id=os.getenv('CHANNEL_ID')
+    channel_id = os.getenv('CHANNEL_ID')
 
     bot = telegram.Bot(token=telegram_bot_token)
 
@@ -36,4 +46,3 @@ if __name__ == '__main__':
             pass
         except TypeError:  # if user put in 'images' non-photo file
             pass
-    
